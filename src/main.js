9bd856291751d802dc8f2cafe7cdb50086dca303
sayoungae <duddo0305@gmail.com>
2024-02-27 15:05:30 +0900

    build 3차

--- src/main.js
+++ src/main.js
@@ -1,27 +1,10 @@
 import "./assets/scss/style.scss";
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
 
-let app = "";
-let containerSelector = "#app";
+const app = createApp(App)
 
-// check if app has been mounted already
-const mountPoint = document.querySelector(containerSelector);
+app.use(router)
 
-if (mountPoint && mountPoint.__vue_app__ !== undefined) {
-
-    // Set the existing mount point to 'app'.
-    app = mountPoint.__vue_app__._instance.proxy;
-}
-else {
-
-    // create a new app instance
-    app = createApp(App);
-    
-    // Install the required instances like plugin, component and directive.
-    app.use(router);
-
-    // Mount 'app' (App.vue) as root component.
-    app.mount(containerSelector);
-}
+app.mount("#app")
