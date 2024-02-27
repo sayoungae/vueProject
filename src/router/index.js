e93ea5fb613d2f8e25f8c5c007795317c5a4421b
sayoungae <duddo0305@gmail.com>
2024-02-27 14:08:37 +0900

    경로수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue';
-import TodaySection from '../components/TodaySection.vue';
-import ChartSection from '../components/ChartSection.vue';
+import HomeView from '/views/HomeView.vue';
+import TodaySection from '/components/TodaySection.vue';
+import ChartSection from '/components/ChartSection.vue';
 
 const routes = [
   {
